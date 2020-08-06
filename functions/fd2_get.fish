2cc349d7b3592de1bffad8e14568910105c8c35d
Andrew Matthews <andrew.matthews@team.telstra.com>
2020-08-07 09:47:10 +1000

    simple text input function

---
+++ functions/fd2_get.fish
@@ -0,0 +1,4 @@
+function fd2_get
+	printf '%s\n' $argv
+read -P '> ' fd2_input
+end
--- functions/fd2_select_from_find.fish
+++ functions/fd2_select_from_find.fish
@@ -1,6 +1,8 @@
 # Defined in /home/d235108/.config/fishdots/home/fishdots-0.1.44/init/rc1.d/03-functions.fish @ line 65
 function fd2_select_from_find --argument root_path pattern
-	set -l opts (fd2_find $root_path $pattern)
-    fd2_select_item $opts
+  set -e fd2_selected_file
+  fd2_select_item (fd2_find $root_path $pattern)
+  if test $status -eq 0
     set -g fd2_selected_file "$fd2_selected_item"
+  end
 end
